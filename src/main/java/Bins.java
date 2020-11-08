972448375c026fbc6bc31d5ecad7f0c5fc7ac211
EricRinger6 <eric.ringer6@gmail.com>
2020-11-08 16:50:47 -0500

    Finished

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class Bins {
+    private int startingSumDice;
+    private int endingSumDice;
+    private Map<Integer, Integer> frequencies;
+
+    public Bins(int startingSumDice, int endingSumDice){
+        this.startingSumDice = startingSumDice;
+        this.endingSumDice = endingSumDice;
+        this.frequencies = new HashMap<Integer, Integer>();
+    }
+
+    public void fillBins(Dice dice, int numOfRolls){
+        for(int y = 0; y < numOfRolls; y++){
+            Integer net = dice.tossAndSum();
+            if(frequencies.containsKey(net)){
+                Integer value = frequencies.get(net);
+                value++;
+                frequencies.put(net, value);
+            }
+            frequencies.put(net, 1);
+        }
+    }
+
+    public Integer getBin(int binNum){
+        Integer binCont = null;
+        for(Map.Entry<Integer, Integer> entry : frequencies.entrySet()){
+            if(frequencies.containsKey(binNum)){
+                binCont = frequencies.get(binNum);
+            }
+            System.out.println("Bins don't contain that number.");
+        }
+        return binCont;
+    }
 
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+
+
 public class Dice {
 
+    private int numOfDice;
 
+    public Dice(int numberOfDice){
+        this.numOfDice = numberOfDice;
+    }
+    public Integer tossAndSum() {
+        int sum = 0;
+        for(int i = 0; i < numOfDice; i++){
+            sum += (int)(Math.random() * 6) + 1;
+        }
+        return sum;
+    }
+
+    public ArrayList<Integer> getDiceValue(){
+        ArrayList<Integer> diceValues = new ArrayList<Integer>();
+        for(int x = 0; x < numOfDice; x++){
+            int values = (int)(Math.random() * 6) + 1;
+            diceValues.add(values);
+        }
+        return diceValues;
+    }
+
+    public int getNumOfDice(){
+        return this.numOfDice;
+    }
+
+    public void setNumOfDice(int numOfDice){
+        this.numOfDice = numOfDice;
+    }
 }
